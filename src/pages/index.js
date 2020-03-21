9c111faf6b17645105f9e75fe5217e3abe61c9ff
Eric Leong <eleong95@gmail.com>
2020-03-20 19:37:41 -0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,43 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+import { orange } from '@material-ui/core/colors';
+
+import purple from '@material-ui/core/colors/purple';
+
+
+import Button from '@material-ui/core/Button';
+
+import Layout from 'components/layout';
+import Image from 'components/image';
+import SEO from 'components/seo';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple
+  }
+});
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  <ThemeProvider theme={theme}>
 
-export default IndexPage
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+        <Image />
+      </div>
+
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  </ThemeProvider>
+);
+
+export default IndexPage;
